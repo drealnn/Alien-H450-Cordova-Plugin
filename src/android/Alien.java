eb365e56556e827c9da923a766d52247868a2c74
drealnn <drealnn@gmail.com>
2017-03-26 01:12:37 -0400

    debugging onkeylistener issue

--- src/android/Alien.java
+++ src/android/Alien.java
@@ -96,15 +96,16 @@ public class Alien extends CordovaPlugin {
 		try {
 			Log.i(TAG, "getting view from webview");
 			this.currentView = webView.getView();
-			Log.i(TAG, "setting the views on key listener");
-			// this.currentView.setOnKeyListener(
-					// new View.OnKeyListener(){
-						// @Override
-						// public boolean onKey(View view, int keyCode, KeyEvent event){
-							// return doKey(view, keyCode, event);
-						// }
-					// }
-			// );
+			this.currentView.setOnKeyListener(
+					new View.OnKeyListener(){
+						@Override
+						public boolean onKey(View view, int keyCode, KeyEvent event){
+							//boolean val = super.onKey(view, keyCode, event);
+							Log.i(TAG, "Calling the onkey event");
+							return doKey(view, keyCode, event);
+						}
+					}
+			);
 		} catch(Exception e){
 			Log.e(TAG, "Error: "+e);
 		}
